82cf065d85dc6f4558998ed62a868dfbe62ae0f4
Zaina <zenataha20@gmail.com>
2024-05-07 00:34:00 +0300

    Add project files.

---
+++ WebApplication1/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplication1.Models;
+
+namespace WebApplication1.Data
+{
+    public class ApplicationDbContext :DbContext
+    {
+        public DbSet<Employee> Employees { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=.;Database=EmpMVC;trusted_connection=true;trustServerCertificate=true");
+        }
+
+    }
+}
